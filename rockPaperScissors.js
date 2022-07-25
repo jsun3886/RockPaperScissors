abaf36a7fbb8769efc00abde36caacd23f0fbb74
Josh Sunday <jsunday86@gmail.com>
2022-07-25 10:14:38 -0400

    Add the getComputerchoice function
    This function returns Rock Paper or
    Scissors in String form as the computers
    move randomly.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,1 +1,10 @@
 console.log("Lets Play Rock Paper Scissors!")
+
+function getComputerChoice(){
+    const rpsOptions =["Rock", "Paper", "Scissors"]
+    return rpsOptions[Math.floor(Math.random()*3)]
+}
+
+for(let  i=0; i<5; i++){
+    console.log(getComputerChoice())
+}
