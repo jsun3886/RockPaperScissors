f0131abba29aa78def61d72460db347bbae798ad
Josh Sunday <jsunday86@gmail.com>
2022-07-25 10:25:58 -0400

    Add playRound to rockPaperScissors.js
    Adds the function that processes the result for a
    Rock Paper Scissors game. The input from the user must be
    Rock, Paper, or Scissors exactly so the function getting the
    player choice should sanatize the user input.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -4,7 +4,33 @@ function getComputerChoice(){
     const rpsOptions =["Rock", "Paper", "Scissors"]
     return rpsOptions[Math.floor(Math.random()*3)]
 }
+function playRound(playerSelection, computerSelection){
+    switch(playerSelection){
+        case "Rock":
+            if (computerSelection=="Paper"){return "You Lose, Paper beats Rock!"}
+            if (computerSelection=="Scissors"){return "You Win!, Rock Beats Scissors"}
+            return "Rock vs Rock, Draw!"
+            break;
 
-for(let  i=0; i<5; i++){
-    console.log(getComputerChoice())
+        case "Paper":
+            if (computerSelection=="Rock"){return "You Win!, Paper beats Rock!"}
+            if (computerSelection=="Scissors"){return "You Lose, Scissors Beats Paper"}
+            return "Paper vs Paper, Draw!"
+            break;
+
+        case "Scissors":
+            if (computerSelection=="Paper"){return "You Win!, Scissors beats Paper!"}
+            if (computerSelection=="Rock"){return "You Lose, Rock Beats Scissors"}
+            return "Scissors vs Scissors, Draw!"
+            break;
+
+        default: return ("player selection input error")
+    }
+
+
+}
+
+
+for ( let i = 0; i<5; i++){
+    console.log(playRound("Rock",getComputerChoice()))
 }
